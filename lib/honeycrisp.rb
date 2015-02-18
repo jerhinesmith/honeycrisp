4b29191e3597af06b2ef575b986837c1875c5471
Justin Rhinesmith <jerhinesmith@gmail.com>
2015-02-18 10:47:01 -0800

    Forgot to require the subscription

--- lib/honeycrisp.rb
+++ lib/honeycrisp.rb
@@ -3,6 +3,7 @@ require File.join(File.dirname(__FILE__), 'honeycrisp', 'configuration')
 require File.join(File.dirname(__FILE__), 'honeycrisp', 'client')
 require File.join(File.dirname(__FILE__), 'honeycrisp', 'receipt')
 require File.join(File.dirname(__FILE__), 'honeycrisp', 'in_app_purchase')
+require File.join(File.dirname(__FILE__), 'honeycrisp', 'subscription')
 require File.join(File.dirname(__FILE__), 'honeycrisp', 'itunes', 'error')
 
 module Honeycrisp
